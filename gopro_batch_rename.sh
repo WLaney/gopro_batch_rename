c9fe8fabd1a0f31f43c09a811dcb55de7708fbec
William Laney <williamlaney@yahoo.com>
2016-07-20 21:08:11 -0400

    added print at end of script

--- gopro_batch_rename.sh
+++ gopro_batch_rename.sh
@@ -11,3 +11,4 @@ newFilename=$(echo $file | sed -E 's/GOPR/GP00/')
 newFilename=$(echo $newFilename | sed -E 's/GP([0-9]{2})([0-9]{4})\.mp4/\2_GP\1\.mp4/')
 mv $file $newFilename 
 done 
+echo "done"
