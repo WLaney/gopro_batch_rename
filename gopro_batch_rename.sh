a42c67cc8627b3cdd31fbdfbfb30cc3684f41956
William Laney <williamlaney@yahoo.com>
2016-07-21 12:09:56 -0400

    removed reduncy in regex

--- gopro_batch_rename.sh
+++ gopro_batch_rename.sh
@@ -8,7 +8,7 @@
 
 for file in *.[Mm][pP]4; do
 newFilename=$(echo $file | sed -E 's/GOPR/GP00/')
-newFilename=$(echo $newFilename | sed -E 's/GP([0-9]{2})([0-9]{4})\.[Mm][Pp]4|.MP4/\2_GP\1\.mp4/')
+newFilename=$(echo $newFilename | sed -E 's/GP([0-9]{2})([0-9]{4})\.[Mm][Pp]4/\2_GP\1\.mp4/')
 mv $file $newFilename 
 done 
 echo "done"
